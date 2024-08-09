6bf98a721c1d3cd6fff3bd951bd2e0cf81f9c9cb
Taiga Sato <taiga.satou@azoom.jp>
2024-08-09 17:28:44 +0900

    Update

--- exec-schemaspy.sh
+++ exec-schemaspy.sh
@@ -5,6 +5,21 @@ else
   echo ".envファイルが見つかりません。"
   exit 1
 fi
+# schemaspy.propertiesファイルの生成
+cat <<EOL > schemaspy.properties
+schemaspy.t=mysql
+schemaspy.dp=./mysql-connector-j-9.0.0.jar
+schemaspy.db=${DATABASE_NAME}
+schemaspy.s=${DATABASE_NAME}
+schemaspy.host=${DATABASE_HOST}
+schemaspy.port=${DATABASE_PORT}
+schemaspy.u=${DATABASE_USER}
+schemaspy.p=${DATABASE_PASSWORD}
+schemaspy.o=${SCHEMASPY_OUTPUT_DIR}
+schemaspy.hq=${SCHEMASPY_DOT_PATH}
+schemaspy.imageformat=svg
+schemaspy.imagefontadjust=true
+EOL
 
 # バージョン情報を引数から取得
 if [ -z "$1" ]; then
@@ -25,3 +40,6 @@ if [ -f "${INDEX_FILE}" ];then
   # .${DATABASE_NAME}を.$VERSIONに置き換え
   sed -i '' "s|<span class=\"navbar-brand\" style=\"padding-left: 0\">.${DATABASE_NAME}</span>|<span class=\"navbar-brand\" style=\"padding-left: 0\">.${VERSION}</span>|" "${INDEX_FILE}"
 fi
+
+# schemaspy.propertiesファイルの削除
+rm -f schemaspy.properties
